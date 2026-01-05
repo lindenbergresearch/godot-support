907281cbcc35c048bfb6fdb542abf4b47a833666
Patrick Lindenberg <heapdump@icloud.com>
2026-01-05 13:05:37 +0100

    add doc, reformat code

--- gdscript/src/main/kotlin/gdscript/GdIcon.kt
+++ gdscript/src/main/kotlin/gdscript/GdIcon.kt
@@ -1,13 +1,28 @@
 package gdscript
 
+import GdScriptPluginIcons
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.util.IconLoader
 import org.jetbrains.annotations.ApiStatus
 import javax.swing.Icon
 
+/**
+ * Handles loading and managing editor icons for classes in the context of Godot plugin development.
+ * The class caches icons to optimize performance and provides a fallback mechanism using a default backup icon
+ * for cases where the desired icon cannot be loaded.
+ */
 object GdIcon {
     private var editorIcons = HashMap<String, Icon>()
 
+
+    /**
+     * Retrieves the editor icon corresponding to the specified class name. If the icon is not already loaded,
+     * it attempts to load the icon from the appropriate resource location and caches it. If the icon cannot be
+     * loaded, a default backup icon is used instead.
+     *
+     * @param className the name of the class for which the editor icon should be retrieved
+     * @return the corresponding editor icon, or a default backup icon if the specific icon cannot be loaded
+     */
     @ApiStatus.Obsolete // todo: find a way to get rid of this completely and also all unused Godot editor icons
     fun getEditorIcon(className: String): Icon {
         val icon = editorIcons[className]
