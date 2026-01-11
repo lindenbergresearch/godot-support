71d3768e3a5ae8162ccef68482e91cba5d5ce440
Patrick Lindenberg <heapdump@icloud.com>
2026-01-11 21:10:02 +0100

    cleanup

--- gdscript/src/main/kotlin/gdscript/psi/utils/GdInheritanceUtil.kt
+++ gdscript/src/main/kotlin/gdscript/psi/utils/GdInheritanceUtil.kt
@@ -6,10 +6,7 @@ import com.intellij.psi.PsiFile
 import com.intellij.psi.util.PsiTreeUtil
 import gdscript.index.impl.GdClassIdIndex
 import gdscript.index.impl.GdFileResIndex
-import gdscript.psi.GdClassDeclTl
-import gdscript.psi.GdClassNaming
-import gdscript.psi.GdFile
-import gdscript.psi.GdInheritance
+import gdscript.psi.*
 import gdscript.utils.VirtualFileUtil.getPsiFile
 
 object GdInheritanceUtil {
@@ -29,13 +26,16 @@ object GdInheritanceUtil {
         }
     }
 
+
     /**
      * @param element GdClassDeclTL|GdClassNaming|GdFile
      *
      * @return GdClassDeclTL|GdFile
      */
-    @Deprecated("Switch to getExtendedElement(element, project) to promote efficient project reference usage",
-            ReplaceWith("getExtendedElement(element, project)", "gdscript.psi.utils.GdInheritanceUtil.getExtendedElement", ))
+    @Deprecated(
+        "Switch to getExtendedElement(element, project) to promote efficient project reference usage",
+        ReplaceWith("getExtendedElement(element, project)", "gdscript.psi.utils.GdInheritanceUtil.getExtendedElement")
+    )
     fun getExtendedElement(element: PsiElement): PsiElement? {
         return getExtendedElement(
             getExtendedClassId(element),
@@ -43,6 +43,7 @@ object GdInheritanceUtil {
             element.project,
         )
     }
+
 
     /**
      * @param element GdClassDeclTL|GdClassNaming|GdFile
@@ -67,6 +68,7 @@ object GdInheritanceUtil {
         return false
     }
 
+
     /**
      * @param classId FQN like MyClass.DataClass or "res://Item.gd"
      *
@@ -75,15 +77,9 @@ object GdInheritanceUtil {
     fun getExtendedElement(classId: String, element: PsiElement, project: Project): PsiElement? {
         val classEl = GdClassUtil.getClassIdElement(classId, element, project)
         // Extending directly named class (includes "res://Item.gd".InnerClass)
-        if (classEl != null) {
-            return if (classEl.parent is GdClassDeclTl) {
-                classEl.parent
-            } else {
-                classEl.containingFile
-            }
-        }
+        classEl?.let { return (it.parent as? GdClassDeclTl) ?: it.containingFile }
 
-        // In case of unnamed "res://Item.gd" check for the resource itself
+        // In the case of unnamed "res://Item.gd" check for the resource itself
         val file = GdFileResIndex.getFiles(classId.trim('"', '\''), project).firstOrNull() ?: return null
 
         return file.getPsiFile(project)
