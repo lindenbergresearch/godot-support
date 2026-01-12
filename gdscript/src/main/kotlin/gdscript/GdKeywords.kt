ff5f86f22529a564c73e6127dc1685af68870a8d
Patrick Lindenberg <heapdump@icloud.com>
2026-01-12 17:24:36 +0100

    add keywords, add docs

--- gdscript/src/main/kotlin/gdscript/GdKeywords.kt
+++ gdscript/src/main/kotlin/gdscript/GdKeywords.kt
@@ -1,10 +1,26 @@
 package gdscript
 
+/**
+ * The `GdKeywords` object serves as a centralized repository for defining constants and keywords
+ * commonly used in the context of the GDScript language. It provides easy access to predefined
+ * strings for various GDScript keywords, constants, math constants, return types, prefixes,
+ * flow types, and annotations.
+ *
+ * This class can be useful for maintaining consistency across a codebase or for use in tools
+ * or utilities that interact with GDScript.
+ */
 object GdKeywords {
+    /** Keywords */
     const val EXTENDS = "extends"
     const val CLASS = "class"
+    const val EMIT = "emit"
     const val SIGNAL = "signal"
+
+    const val LOAD = "load"
+    const val PRELOAD = "preload"
+
     const val CLASS_NAME = "class_name"
+    const val ONREADY = "onready"
     const val FUNC = "func"
     const val STATIC = "static"
     const val VARARG = "vararg"
@@ -20,15 +36,25 @@ object GdKeywords {
     const val SUPER = "super"
     const val VARIANT = "Variant"
 
+
     /** Constants */
     const val TRUE = "true"
     const val FALSE = "false"
     const val NULL = "null"
+
+
+    /** Math Constants */
     const val NAN = "NAN"
     const val INF = "INF"
     const val TAU = "TAU"
     const val PI = "PI"
-    val MATH_CONSTANTS = setOf(PI, TAU, INF, NAN)
+    val MATH_CONSTANTS = setOf(
+        PI,
+        TAU,
+        INF,
+        NAN
+    )
+
 
     /** Return types */
     const val INT = "int"
@@ -39,17 +65,29 @@ object GdKeywords {
     const val DICTIONARY = "Dictionary"
     const val STRING_NAME = "StringName"
     const val NODE_PATH = "NodePath"
-    val BUILT_TYPES = setOf(INT, STRING, FLOAT, BOOL, ARRAY, DICTIONARY)
     const val VOID = "void"
     const val CALLABLE = "Callable"
 
+    val BUILT_TYPES = setOf(
+        INT,
+        STRING,
+        FLOAT,
+        BOOL,
+        ARRAY,
+        DICTIONARY
+    )
+
+
     /** Prefixes */
     const val RESOURCE_PREFIX = "res://"
+    const val SPECIAL_NAME_PREFIX = "_"
+    const val PATH_SEPARATOR = "/"
 
-    const val GLOBAL_SCOPE = "_GlobalScope"
-    const val GLOBAL_GD_SCRIPT = "_GDScript"
 
-    const val INIT_METHOD = "_init"
+    const val GLOBAL_SCOPE = "${SPECIAL_NAME_PREFIX}GlobalScope"
+    const val GLOBAL_GD_SCRIPT = "${SPECIAL_NAME_PREFIX}GDScript"
+    const val INIT_METHOD = "${SPECIAL_NAME_PREFIX}init"
+
 
     /** Flow Types */
     const val FLOW_RETURN = "return"
@@ -57,11 +95,13 @@ object GdKeywords {
     val LITERALS = arrayOf(TRUE, FALSE, NULL)
     val LITERAL_TYPES = arrayOf(BOOL, BOOL, NULL)
 
+
     /** Annotations */
     const val ANNOTATION_TOOL = "tool"
     const val ANNOTATION_EXPORT = "export"
     const val ANNOTATION_ONREADY = "onready"
     const val ANNOTATION_ICON = "icon"
+
 
     // TODO delete - use?
     val ANNOTATIONS_ROOT_ONLY = arrayOf(
